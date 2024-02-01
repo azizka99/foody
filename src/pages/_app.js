6e16a78867682b9dac22f423ed53478e72fbaf26
azizka_99 <azizka.ibragimov@gmail.com>
2024-02-01 23:27:44 +0400

    changes

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,10 @@
 import "@/styles/globals.css";
+import {IntlProvider} from '';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+      <IntlProvider>
+        <Component {...pageProps} />
+      </IntlProvider>
+  );
 }
